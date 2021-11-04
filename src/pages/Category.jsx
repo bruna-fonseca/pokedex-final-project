6910769ad5d44bd3f7c476bd0d46b88304dfc227
Aieychan01 <87831895+Aieychan01@users.noreply.github.com>
2021-11-04 19:45:52 -0300

    envio da requisição

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 export default function Category() {
+  const [categorias, setCategorias] = useState([]);
+  function catGrass() {
+    axios.get('https://pokeapi.co/api/v2/type/12/')
+      .then(
+        (response) => {
+          setCategorias(response.data.pokemon)
+        }
+      )
+      .catch((e) => {
+        console.log(e)
+      }
+      );
+  }
+
+  function catFire() {
+    axios.get('https://pokeapi.co/api/v2/type/10/')
+      .then(
+        (response) => {
+          setCategorias(response.data.pokemon)
+        }
+      )
+      .catch((e) => {
+        console.log(e)
+      }
+
+      );
+  }
+
+  function catWater() {
+    axios.get('https://pokeapi.co/api/v2/type/11/')
+      .then(
+        (response) => {
+          setCategorias(response.data.pokemon)
+        }
+      )
+      .catch(
+        (e) => {
+          console.log(e)
+        }
+      );
+  }
+
+  function catRaio() {
+    axios.get('https://pokeapi.co/api/v2/type/13/')
+      .then(
+        (response) => {
+          setCategorias(response.data.pokemon)
+        }
+      )
+      .catch(
+        (e) => {
+          console.log(e)
+        }
+      );
+  }
   return (
     <div>
-      <h1>Tela de categoria</h1>
+      
     </div>
   );
 };
