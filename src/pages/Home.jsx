9e7df1d901b0af7be326293bf7fbccfb2945102b
cristyandarlen <christyandarllem.2021@gmail.com>
2021-11-05 23:45:44 -0300

    envio da tela home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
-
+import NavBar from '../components/NavBar';
+import '../styles/Home.css';
+import pikachu from '../assets/home-bg.png';
+import { StyledButton } from '../styles/styles';
 export default function Home() {
   return (
-    <div>
-      <h1>Tela de home</h1>
+    <div className='conteudo'>
+      <NavBar />
+      <div className='pai' >
+        <div className='filho'>
+          <div> <p className="title"><strong>Encontre</strong> Todos<br></br>
+            os seus pokémons<br></br> favoritos</p>
+          </div>
+          <div className="content2">
+            <p className="title2">
+              Você pode aprender sobre o tipo<br></br>
+              de pokémon, suas forças,<br></br>
+              fraquezas e habilidades</p>
+          </div>
+          < StyledButton className="botao">Veja os pokemóns</StyledButton>
+        </div>
+
+        <img className="pikachu" src={pikachu} alt="pikachu" />
+      </div>
     </div>
+
   );
 };
